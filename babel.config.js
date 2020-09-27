45984715fd2bc39bbb987f02c3d40eaf4c696016
Iris-97 <kkzhuzai@126.com>
2020-09-27 08:47:32 +0800

    first

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  //独立安装的插件
+   plugins: [
+    ['import', {
+      libraryName: 'vant',
+      libraryDirectory: 'es',
+      style: true
+    }, 'vant']
+  ] 
 }
