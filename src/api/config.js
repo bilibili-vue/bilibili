406d7ba68b80cf2b6984e93637e70650d24b2458
yasepandelag <595791554@qq.com>
2020-10-08 18:07:29 +0800

    1807

---
+++ src/api/config.js
@@ -0,0 +1,210 @@
+
+export const category = [
+    {
+      name: '首页',
+      rid: 0
+    },
+    {
+      name: '动画',
+      rid: 1,
+      blocks: [
+        { name: '推荐', rid: 1 },
+        { name: 'MAD·AMV', rid: 24 },
+        { name: 'MMD·3D', rid: 25 },
+        { name: '短片·手书·配音', rid: 47 },
+        { name: '特摄', rid: 86 },
+        { name: '综合', rid: 27 }
+      ]
+    },
+    {
+      name: '番剧',
+      rid: 13,
+      blocks: [
+        { name: '推荐', rid: 13 },
+        { name: '连载动画', rid: 33 },
+        { name: '完结动画', rid: 32 },
+        { name: '资讯', rid: 51 },
+        { name: '官方延伸', rid: 152 }
+      ]
+    },
+    {
+      name: '国创',
+      rid: 167,
+      blocks: [
+        { name: '推荐', rid: 167 },
+        { name: '国产动画', rid: 153 },
+        { name: '国产原创相关', rid: 168 },
+        { name: '布袋戏', rid: 169 },
+        { name: '动态漫·广播剧', rid: 195 },
+        { name: '资讯', rid: 170 }
+      ]
+    },
+    {
+      name: '音乐',
+      rid: 3,
+      blocks: [
+        { name: '推荐', rid: 3 },
+        { name: '原创音乐', rid: 28 },
+        { name: '翻唱', rid: 31 },
+        { name: 'VOCALOID·UTAU', rid: 30 },
+        { name: '电音', rid: 194 },
+        { name: '演奏', rid: 59 },
+        { name: 'MV', rid: 193 },
+        { name: '音乐现场', rid: 29 },
+        { name: '音乐综合', rid: 130 }
+      ]
+    },
+    {
+      name: '舞蹈',
+      rid: 129,
+      blocks: [
+        { name: '推荐', rid: 129 },
+        { name: '宅舞', rid: 20 },
+        { name: '街舞', rid: 198 },
+        { name: '明星舞蹈', rid: 199 },
+        { name: '中国舞', rid: 200 },
+        { name: '舞蹈综合', rid: 154 },
+        { name: '舞蹈教程', rid: 156 }
+      ]
+    },
+    {
+      name: '游戏',
+      rid: 4,
+      blocks: [
+        { name: '推荐', rid: 4 },
+        { name: '单机游戏', rid: 17 },
+        { name: '电子竞技', rid: 171 },
+        { name: '手机游戏', rid: 172 },
+        { name: '网络游戏', rid: 65 },
+        { name: '桌游棋牌', rid: 173 },
+        { name: 'GMV', rid: 121 },
+        { name: '音游', rid: 136 },
+        { name: 'Mugen', rid: 19 }
+      ]
+    },
+    {
+      name: '科技',
+      rid: 36,
+      blocks: [
+        { name: '推荐', rid: 36 },
+        { name: '科学科普', rid: 201 },
+        { name: '社科人文', rid: 124 },
+        { name: '野生技术协会', rid: 122 },
+        { name: '演讲·公开课', rid: 39 },
+        { name: '星海', rid: 96 },
+        { name: '机械', rid: 98 },
+        { name: '汽车', rid: 176 }
+      ]
+    },
+    {
+      name: '数码',
+      rid: 188,
+      blocks: [
+        { name: '推荐', rid: 188 },
+        { name: '手机平板', rid: 95 },
+        { name: '电脑装机', rid: 189 },
+        { name: '摄影摄像', rid: 190 },
+        { name: '影音智能', rid: 191 }
+      ]
+    },
+    {
+      name: '生活',
+      rid: 160,
+      blocks: [
+        { name: '推荐', rid: 160 },
+        { name: '搞笑', rid: 138 },
+        { name: '日常', rid: 21 },
+        { name: '美食圈', rid: 76 },
+        { name: '动物圈', rid: 75 },
+        { name: '手工', rid: 161 },
+        { name: '绘画', rid: 162 },
+        { name: '运动', rid: 163 },
+        { name: '其他', rid: 174 }
+      ]
+    },
+    {
+      name: '鬼畜',
+      rid: 119,
+      blocks: [
+        { name: '推荐', rid: 119 },
+        { name: '鬼畜调教', rid: 22 },
+        { name: '音MAD', rid: 26 },
+        { name: '人力VOCALOID', rid: 126 },
+        { name: '教程演示', rid: 127 }
+      ]
+    },
+    {
+      name: '时尚',
+      rid: 155,
+      blocks: [
+        { name: '推荐', rid: 155 },
+        { name: '美妆', rid: 157 },
+        { name: '服饰', rid: 158 },
+        { name: '健身', rid: 164 },
+        { name: 'T台', rid: 159 },
+        { name: '风尚标', rid: 192 }
+      ]
+    },
+    {
+      name: '娱乐',
+      rid: 5,
+      blocks: [
+        { name: '推荐', rid: 5 },
+        { name: '综艺', rid: 71 },
+        { name: '明星', rid: 137 },
+        { name: 'Korea相关', rid: 131 }
+      ]
+    },
+    {
+      name: '影视',
+      rid: 181,
+      blocks: [
+        { name: '推荐', rid: 181 },
+        { name: '影视杂谈', rid: 182 },
+        { name: '影视剪辑', rid: 183 },
+        { name: '短片', rid: 85 },
+        { name: '预告·资讯', rid: 184 }
+      ]
+    },
+    {
+      name: '广告',
+      rid: 165,
+      blocks: [
+        { name: '推荐', rid: 165 }
+      ]
+    },
+    {
+      name: '纪录片',
+      rid: 177,
+      blocks: [
+        { name: '推荐', rid: 177 },
+        { name: '人文·历史', rid: 37 },
+        { name: '科学·探索·自然', rid: 178 },
+        { name: '军事', rid: 179 },
+        { name: '社会·美食·旅行', rid: 180 }
+      ]
+    },
+    {
+      name: '电影',
+      rid: 23,
+      blocks: [
+        { name: '推荐', rid: 23 },
+        { name: '华语电影', rid: 147 },
+        { name: '欧美电影', rid: 145 },
+        { name: '日本电影', rid: 146 },
+        { name: '其他国家', rid: 83 }
+      ]
+    },
+    {
+      name: '电视剧',
+      rid: 11,
+      blocks: [
+        { name: '推荐', rid: 11 },
+        { name: '国产剧', rid: 185 },
+        { name: '海外剧', rid: 187 }
+      ]
+    },
+    { name: '直播', rid: -1 },
+    { name: '相簿', rid: -2 }
+  ]
+  
---
+++ src/components/common/lunbo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="lunbo">
+    <van-swipe :autoplay="3000">
+      <van-swipe-item v-for="(image, index) in images" :key="index">
+        <img v-lazy="image" />
+      </van-swipe-item>
+    </van-swipe>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import { Swipe, SwipeItem, Lazyload } from "vant";
+
+Vue.use(Swipe);
+Vue.use(SwipeItem);
+Vue.use(Lazyload);
+
+export default {
+  data() {
+    return {
+      images: [
+        "https://i0.hdslb.com/bfs/archive/0877cdd4b401b66d578841ad0cebbf1dec95fe07.jpg@480w_270h",
+        "https://i0.hdslb.com/bfs/archive/1de7300e9793ee713c8e6515df9439378dd8c790.jpg@480w_270h",
+        "https://i0.hdslb.com/bfs/archive/9e0ffb1650d9b68ae61bf02a18bf5892f9902890.jpg@480w_270h",
+        "https://i0.hdslb.com/bfs/archive/68cb764e2a9060623f2678928588fd1d5b3aae75.png@480w_270h",
+      ],
+    };
+  },
+};
+</script>
+
+<style lang='stylus' scoped>
+.my-swipe .van-swipe-item {
+  color: #fff;
+  font-size: 0.2rem;
+  line-height: 0.5rem;
+  text-align: center;
+  background-color: #39a9ed;
+}
+</style>
