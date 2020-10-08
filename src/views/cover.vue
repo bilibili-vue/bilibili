5c27dac6816b15e18e1ac3d9b620ed97b6588b54
yasepandelag <595791554@qq.com>
2020-10-08 19:49:27 +0800

    1949

--- src/views/cover.vue
+++ src/views/cover.vue
@@ -5,7 +5,7 @@
               <!--  <img src="../assets/logo.png"  alt="" style="width:100%;height:27.778vw;"> -->
                <img :src="detailitem.pic"  alt="" style="width:100%;height:27.778vw;">
               <div class="bottom">
-                  <div class="icon-play2"><span class="video">{{detailitem.stat.view}}</span></div>
+                  <div class="icon-play2"><span class="video">{{detailitem.stat.view>10000?(detailitem.stat.view/10000).toFixed(1)+"万":detailitem.stat.view}}</span></div>
                   <div class="icon-file-text"> <span class="comment">{{!detailitem.stat.danmaku ? 66 : detailitem.stat.danmaku}}</span> </div>
               </div>
           </div>
