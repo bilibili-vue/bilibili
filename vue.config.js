2c46a957414d1d198ccfe2d94b19c67953a0c0c3
Iris-97 <kkzhuzai@126.com>
2020-10-08 14:09:12 +0800

    10-08-01

--- vue.config.js
+++ vue.config.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-const { default: Axios } = require('axios')
-const path = require('path')
-
-
-module.exports={
-    devServer: {
-        proxy: {
-            '/proxyApi': {
-                target: 'http://api.bilibili.cn',
-                changeOrigin: true,
-                pathRewrite: {
-                  '^/proxyApi': ''
-                }
-              },
-            '/api': {
-                target: 'http://www.bilibili.com',
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/api': ''
-                }
-            }
-        }
-    }
-}
-=======
-/* webpack也是基于node开发的 ，用webpack在当前环境直接做做代理，不用node
-
-
-
-*/
 
 module.exports = {
     devServer: {
@@ -57,4 +26,3 @@ module.exports = {
       }
     }
   }
->>>>>>> master
